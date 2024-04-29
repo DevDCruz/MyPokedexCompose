84e66b695d84849f98b281c3ce7973dffe84390a
DevDCruz <juandecruzsan@gmail.com>
2024-04-29 17:50:22 +0200

    LocationServices and Permissions requeriment

--- app/src/main/java/com/example/mypokedexcompose/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/mypokedexcompose/ui/screens/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.mypokedexcompose.ui.screens.home
 
 
+import android.Manifest
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -22,16 +23,27 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.example.mypokedexcompose.R
 import com.example.mypokedexcompose.data.Pokemon
 import com.example.mypokedexcompose.data.pokemons
+import com.example.mypokedexcompose.ui.common.PermissionRequestEffect
+import com.example.mypokedexcompose.ui.common.getRegion
 import com.example.mypokedexcompose.ui.theme.MyPokedexComposeTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun Screen(content: @Composable () -> Unit) {
@@ -48,13 +60,33 @@ fun Screen(content: @Composable () -> Unit) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(onClick: (Pokemon) -> Unit) {
+
+    val ctx = LocalContext.current.applicationContext
+    val appName = stringResource(id = R.string.app_name)
+    var appBarTitle by remember { mutableStateOf(appName) }
+    val coroutineScope = rememberCoroutineScope()
+
+
+    PermissionRequestEffect(permission = Manifest.permission.ACCESS_COARSE_LOCATION) { granted ->
+        if (granted) {
+            coroutineScope.launch {
+                val region = ctx.getRegion()
+                appBarTitle = "$appName - ($region)"
+            }
+
+        } else {
+            appBarTitle = "$appName (Permission Denied)"
+        }
+
+    }
+
     Screen {
         var scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
         Scaffold(
             topBar = {
                 TopAppBar(
-                    title = { Text(text = "Pokedex") },
+                    title = { Text(text = appBarTitle) },
                     scrollBehavior = scrollBehavior
                 )
             },
@@ -66,7 +98,7 @@ fun HomeScreen(onClick: (Pokemon) -> Unit) {
                 contentPadding = padding
             ) {
                 items(pokemons) { pokemon ->
-                    PokemonItem(pokemon = pokemon, onClick = { onClick(pokemon)})
+                    PokemonItem(pokemon = pokemon, onClick = { onClick(pokemon) })
                 }
             }
         }
