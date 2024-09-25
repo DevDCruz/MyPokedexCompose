3571a824b030c518dd9e9b6790ba178d6d2f7bec
Decruz <juandecruzsan@gmail.com>
2024-09-25 19:53:19 +0200

    Results changed fixed without savePokemons in PokemonRepository

--- app/src/main/java/com/example/mypokedexcompose/data/pokemon/PokemonRepository.kt
+++ app/src/main/java/com/example/mypokedexcompose/data/pokemon/PokemonRepository.kt
@@ -2,7 +2,6 @@ package com.example.mypokedexcompose.data.pokemon
 
 import com.example.mypokedexcompose.data.dataSource.local.PokemonLocalDataSource
 import com.example.mypokedexcompose.data.dataSource.remote.pokemon.PokemonRemoteDataSource
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.transform
 import java.util.Random
@@ -12,21 +11,14 @@ class PokemonRepository(
     private val pokemonLocalDataSource: PokemonLocalDataSource
 ) {
 
-    suspend fun fetchPokemon(name: String): Flow<Pokemon?> =
-        pokemonLocalDataSource.getPokemonByName(name).transform { localPokemon ->
-            val pokemon = localPokemon ?: pokemonRemoteDataSource.fetchPokemon(name).also {
-                pokemonLocalDataSource.savePokemon(listOf(it))
-            }
-            emit(pokemon)
-        }
+    fun fetchPokemon(name: String): Flow<Pokemon?> {
+        return pokemonLocalDataSource.getPokemonByName(name)
+    }
 
     suspend fun fetchRandomPokemon(): Flow<Pokemon?> =
         pokemonLocalDataSource.getPokemonById(generateRandomId()).transform { localPokemon ->
             val pokemon =
                 localPokemon ?: pokemonRemoteDataSource.fetchRandomPokemon(generateRandomId())
-                    .also {
-                        pokemonLocalDataSource.savePokemon(listOf(it))
-                    }
             emit(pokemon)
         }
 
