c0d232f910582edd421323e66b5c5f37384ebf81
Decruz <juandecruzsan@gmail.com>
2024-12-05 11:32:05 +0100

    Changes for di refactored

--- app/src/main/java/com/example/mypokedexcompose/ui/screens/pokedex/PokedexScreen.kt
+++ app/src/main/java/com/example/mypokedexcompose/ui/screens/pokedex/PokedexScreen.kt
@@ -44,7 +44,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.example.mypokedexcompose.R
 import com.example.mypokedexcompose.domain.pokemon.PokemonDomain
