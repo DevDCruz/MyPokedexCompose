5307189cf08104e8d146995ee47590ab64eb076f
Decruz <juandecruzsan@gmail.com>
2024-12-06 13:10:15 +0100

    hilt implementation

--- app/src/main/java/com/example/mypokedexcompose/MainActivity.kt
+++ app/src/main/java/com/example/mypokedexcompose/MainActivity.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.mypokedexcompose.ui.screens.Navigation
-import com.example.mypokedexcompose.ui.theme.MyPokedexComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
